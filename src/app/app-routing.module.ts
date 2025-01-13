293258a13d4cd974777dc2bf53a9288b6a6811bc
parvat singh <sunilsingh0639@gmail.com>
2025-01-13 22:44:02 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,41 +19,41 @@ const routes: Routes = [
             (mod) => mod.DashboardModule
           ),
       },
-      {
-        path: 'home',
-        loadChildren: () =>
-          import('./feature/home/home.module').then((mod) => mod.HomeModule),
-      },
+      // {
+      //   path: 'home',
+      //   loadChildren: () =>
+      //     import('./feature/home/home.module').then((mod) => mod.HomeModule),
+      // },
       // {
       //   path: 'notification',
       //   loadChildren: () =>
       //     import('./feature/notification').then((mod) => mod.NotiicfationComponent),
       // },
       
-      {
-        path: 'users',
-        loadChildren: () =>
-          import('./feature/users/users.module').then((mod) => mod.UsersModule),
-      },
-      {
-        path: 'transactions',
-        loadChildren: () =>
-          import('./feature/transactions/transactions.module').then((mod) => mod.TransactionsModule),
-      },
-      {
-        path: 'subscriptions',
-        loadChildren: () =>
-          import('./feature/subscriptions/subscriptions.module').then(
-            (mod) => mod.SubscriptionsModule
-          ),
-      },
-      {
-        path: 'clients',
-        loadChildren: () =>
-          import('./feature/clients/clients.module').then(
-            (mod) => mod.ClientsModule
-          )
-      },
+      // {
+      //   path: 'users',
+      //   loadChildren: () =>
+      //     import('./feature/users/users.module').then((mod) => mod.UsersModule),
+      // },
+      // {
+      //   path: 'transactions',
+      //   loadChildren: () =>
+      //     import('./feature/transactions/transactions.module').then((mod) => mod.TransactionsModule),
+      // },
+      // {
+      //   path: 'subscriptions',
+      //   loadChildren: () =>
+      //     import('./feature/subscriptions/subscriptions.module').then(
+      //       (mod) => mod.SubscriptionsModule
+      //     ),
+      // },
+      // {
+      //   path: 'clients',
+      //   loadChildren: () =>
+      //     import('./feature/clients/clients.module').then(
+      //       (mod) => mod.ClientsModule
+      //     )
+      // },
       {
         path: 'operations',
         loadChildren: () =>
@@ -68,28 +68,28 @@ const routes: Routes = [
             (mod) => mod.ProfileModule
           ),
       },
-      {
-        path: 'audit-logs',
-        loadChildren: () =>
-          import('./feature/audit-logs/audit-logs.module').then(
-            (mod) => mod.AuditLogsModule
-          ),
-      },
-      {
-        path: 'roles',
-        loadChildren: () =>
-          import('./feature/roles/roles.module').then((mod) => mod.RolesModule),
-      },
-      {
-        path: 'subscribers',
-        loadChildren: () =>
-          import('./feature/subscribers/subscribers.module').then((mod) => mod.SubscribersModule),
-      },
-      {
-        path: 'reports',
-        loadChildren: () =>
-          import('./feature/reports/reports.module').then((mod) => mod.ReportsModule),
-      }
+      // {
+      //   path: 'audit-logs',
+      //   loadChildren: () =>
+      //     import('./feature/audit-logs/audit-logs.module').then(
+      //       (mod) => mod.AuditLogsModule
+      //     ),
+      // },
+      // {
+      //   path: 'roles',
+      //   loadChildren: () =>
+      //     import('./feature/roles/roles.module').then((mod) => mod.RolesModule),
+      // },
+      // {
+      //   path: 'subscribers',
+      //   loadChildren: () =>
+      //     import('./feature/subscribers/subscribers.module').then((mod) => mod.SubscribersModule),
+      // },
+      // {
+      //   path: 'reports',
+      //   loadChildren: () =>
+      //     import('./feature/reports/reports.module').then((mod) => mod.ReportsModule),
+      // }
     ],
   },
 ];
